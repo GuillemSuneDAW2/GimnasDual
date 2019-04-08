4e16f82e7f47cf231f2b7b4ee8d788082e138669
Guillemsm97 <32685124+Guillemsm97@users.noreply.github.com>
2019-04-08 20:23:46 +0200

    Retrofit

---
+++ app/src/main/java/com/example/gimnasdual/remote/APIService.java
@@ -0,0 +1,13 @@
+package com.example.gimnasdual.remote;
+
+public interface APIService {
+    /*
+    @FormUrlEncoded
+    @POST("api/users")
+    Call<ResponseRegister> registerEmployee(@Field("name") String name,
+                                            @Field("job") String job);
+
+    @GET("api/users")
+    Call<ResponseClients> getClients(@Query("page") String page);
+     */
+}
